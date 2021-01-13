03b48871121483909162c465fa760efe58c4549f
akito-chan <0.akito.chat.0@gmail.com>
2021-01-13 17:06:01 +0700

    а

---
+++ щща.py
@@ -0,0 +1,43 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from random import randint
+
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.pushButton.clicked.connect(self.circle)
+
+        self.label = QLabel()
+        canvas = QPixmap(800, 610)
+        self.label.setPixmap(canvas)
+
+        layout = QGridLayout(self.centralwidget)
+        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
+        layout.addWidget(self.label, 1, 0)
+
+    def circle(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        w = randint(10, 100)
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor("yellow"))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, w)
+        painter.end()
+        self.update()
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    sys.exit(app.exec_())
